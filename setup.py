1fe15c43047a794a54af87d2bb673220ee9ab4ab
Naoko Kurata <kurata.naoko.jp@gmail.com>
2018-02-22 15:21:31 -0500

    updated setup.py to be fancier

--- setup.py
+++ setup.py
@@ -3,7 +3,11 @@ setup(
     name="helloworld",
     version="0.1",
     packages=find_packages(),
+    author="Naoko kurata",
+    license="GPLv3",
+    description="A package for saying hello",
     entry_points={
-        'console_scripts': ['helloworld = helloworld.__main__:main']
-        }
+        'console_scripts': ['helloworld = helloworld.__main__:main'],
+        },
+    classifiers=["Programming Language :: Python :: 3"],
 )
